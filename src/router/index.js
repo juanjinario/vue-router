c0586599cd0974a121cce3222ec2b03268304fec
juanjinario <juanjco2@gmail.com>
2023-02-03 15:47:12 +0100

    feat: add first route Pokemon

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  // Esto es para evitar el #, que era de versiones anteriores
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/about",
+      name: "about",
       component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    },
+    {
+      path: "/pokemon",
+      name: "pokemon",
+      component: () => import('../views/PokemonView.vue')
+    },
+  ],
+});
 
-export default router
+export default router;
